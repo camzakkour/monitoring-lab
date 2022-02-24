1367fb5d1bbab2db7c837122c1825d77bb0c7d7e
camzakkour <camzakkour@gmail.com>
2022-02-24 14:52:50 -0800

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../handouts/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-app.use(express.static(path.join(__dirname, '../handouts')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 const port = process.env.PORT || 4546
 app.listen(port, () => console.log(`Take us to warp ${port}`))
