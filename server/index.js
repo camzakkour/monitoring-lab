bbf99ab4325dab35e879831a3279a2bc56e26da9
camzakkour <camzakkour@gmail.com>
2022-02-24 14:49:09 -0800

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'fbbcbd4dfb494471a712c664462b6a09',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../handouts/index.html'))
+})
+
+app.use(express.static(path.join(__dirname, '../handouts')))
+
+const port = process.env.PORT || 4546
+app.listen(port, () => console.log(`Take us to warp ${port}`))
